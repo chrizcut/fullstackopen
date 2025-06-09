cf11b146c53664ecf166a64f60b41359eb11fbc1
chrizcut <christine_cutting@msn.com>
2025-06-09 14:09:42 +0200

    Added database to the phonebook

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,50 +1,53 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 app.use(express.json())
 
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
+
+const Person = require('./models/person')
 
 app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`)
+  Person.countDocuments()
+    .then(count => {
+      response.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${new Date()}</p>
+      `);
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => response.json(persons)
+  )
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(person => response.json(person))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -54,13 +57,13 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-//   const maxId = persons.length > 0
-//     ? Math.max(...persons.map(n => Number(n.id)))
-//     : 0
-//   return String(maxId + 1)
-    return Number.parseInt(Math.random()*1_000_000).toString()
-}
+// const generateId = () => {
+// //   const maxId = persons.length > 0
+// //     ? Math.max(...persons.map(n => Number(n.id)))
+// //     : 0
+// //   return String(maxId + 1)
+//     return Number.parseInt(Math.random()*1_000_000).toString()
+// }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -71,24 +74,23 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person=>person.name===body.name)){
-    return response.status(400).json({
-        error:'Name already exists'
-    })
-  }
+  // if (persons.find(person=>person.name===body.name)){
+  //   return response.status(400).json({
+  //       error:'Name already exists'
+  //   })
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson=>
+    response.json(savedPerson)
+  )
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
